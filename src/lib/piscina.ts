051f12a82d7b8d2a521c6b7aec6c7916a7a1c8aa
Nikola Hristov <nikola@nikolahristov.tech>
2022-11-17 22:13:08 +0200

    lean

--- src/lib/piscina.ts
+++ src/lib/piscina.ts
@@ -6,14 +6,12 @@ const __filename = fileURLToPath(import.meta.url);
 
 const calculation = new Piscina({
 	filename: resolve(`${dirname(__filename)}/../../dist/test/calculation.js`),
-	maxQueue: "auto",
 });
 
 const functionCall = new Piscina({
 	filename: resolve(
 		`${dirname(__filename)}/../../dist/test/function-call.js`
 	),
-	maxQueue: "auto",
 });
 
 await Promise.all([
