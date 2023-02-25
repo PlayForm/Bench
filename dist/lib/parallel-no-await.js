bfe4e31d2ef1ba8b683a04b7f161580405bc3bf2
Nikola Hristov <nikola@nikolahristov.tech>
2023-02-25 14:33:40 +0200

    squash!

---
+++ dist/lib/parallel-no-await.js
@@ -0,0 +1,1 @@
+import o from"../test/calculation.js";import a from"../test/function-call.js";await Promise.all([o(),a(),o(),a(),o(),a(),o(),a(),o(),a(),o(),a(),o(),a(),o(),a(),o(),a(),o(),a(),o(),a(),o(),a(),o(),a(),o(),a(),o(),a(),o(),a(),o(),a(),o(),a(),o(),a(),o(),a(),o(),a(),o(),a(),o(),a(),o(),a(),o(),a(),o(),a(),o(),a(),o(),a(),o(),a(),o(),a()]);
---
+++ dist/test/function-call.js
@@ -0,0 +1,1 @@
+var n=async()=>(console.info("This is a function call."),!0);export{n as default};
