ccaaffbaf6ed5203518e87115358ffb7cf61b43a
Nikola Hristov <nikola@nikolahristov.tech>
2023-03-04 21:34:59 +0200

    squash!

--- src/lib/piscina-no-await.ts
+++ src/lib/piscina-no-await.ts
@@ -7,7 +7,7 @@ const __filename = fileURLToPath(import.meta.url);
 const calculation = new Piscina({
 	filename: resolve(`${dirname(__filename)}/../../dist/test/calculation.js`),
 	// rome-ignore lint/nursery/noPrecisionLoss:
-	concurrentTasksPerWorker: 10,
+	concurrentTasksPerWorker: 10.0,
 });
 
 const functionCall = new Piscina({
@@ -15,7 +15,7 @@ const functionCall = new Piscina({
 		`${dirname(__filename)}/../../dist/test/function-call.js`
 	),
 	// rome-ignore lint/nursery/noPrecisionLoss:
-	concurrentTasksPerWorker: 10,
+	concurrentTasksPerWorker: 10.0,
 });
 
 await Promise.all([
